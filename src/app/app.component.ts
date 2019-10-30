03d6f47b60d3839512671ea5cdb7a380e97944be
viveksagacito <vivek.sharma@sagacito.com>
2019-10-30 15:07:06 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  todoArray = [];
+
+  addTodo(value) {
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
+}
